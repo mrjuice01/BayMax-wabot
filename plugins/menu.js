9902729cc92603814155c6cfd52ecf565f7b318a
Toshiro  Hitsugaya <91844428+ToshiroHitsugaya223@users.noreply.github.com>
2022-02-12 22:40:31 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -5,8 +5,9 @@ let levelling = require('../lib/levelling')
 const thumb = fs.readFileSync('./src/thumb.jpg')
 let tags = {
   'main': 'MAIN',
-  'info': 'INFO',
+  'rpg': 'Epic RPG',
   'game': 'GAME',
+  'info': 'INFO',
   'xp': 'EXP & LIMIT',
   'sticker': 'STICKER',
   'admin': 'ADMIN',
@@ -148,7 +149,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    conn.send2ButtonImg(m.chat, thumb, text.trim(), 'BayMax', '-owner', 'rules', '.rules', m)
+    conn.send2ButtonImg(m.chat, thumb, text.trim(), '©BayMax', '-owner', 'rules', '.rules', m)
   } catch (e) {
     conn.reply(m.chat, 'Sorry, the menu is in error', m)
     throw e
