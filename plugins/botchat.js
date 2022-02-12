8dc4c6175ec80d90830f9f2acb40faac8035a83b
Toshiro  Hitsugaya <91844428+ToshiroHitsugaya223@users.noreply.github.com>
2022-02-12 23:31:47 +0530

    Update botchat.js

--- plugins/botchat.js
+++ plugins/botchat.js
@@ -1,11 +1,11 @@
 let handler  = async (m, { conn }) => {
  let name = conn.getName(m.sender)
  let teks = `
- ${pickRandom([` _Hii @${m.sender.split`@`[0]}_\n\n_Saya disini_`, `_Oeetss @${m.sender.split`@`[0]}_\n \n_Ada apa??_`, `_Ekhheemmm, Uhuk Ughhuk_`, `_Puuuiyy_`, `_Poom_`, `_Hadirrr_`, `_Yooossshhhh_`])}
+ ${pickRandom([` Ohh😒','Bahut Achhaa 😂😂😂','🤣🤣','Sahi Hai 🤭🤭🤭🤭🤭','Uffff 😒😒😒😒','😭','Bhery Sed 😖😖😖','Hmmm 😊','Tere jaise yaar kaha 😀','🤣','Ye Dil 💔 Kyu toda 😒','Oyee hoyeee 🤩🤩😍', '😍😍😍😍','😇','Baharo 🤣 Phool Barsao 🌸 Mera Mehbub Aaya Hai 😂','😆Kya se kya ho gye dekhte dekhte😆','Tu Mat Bol Tu Matlabi hai 🤨','😅😅😅','Maine Aapka Photo 📸 Le liya h 😏 Ab Aapko Viral Kar Dunga 😏','Pehele Mera Dost bhi Aise Hi harkate kiya karta tha 😂 Phir Ek din usko Kuch log utha kar le gye','Dil ♥️ Ka Dariya 🌊 Beh Hi Gaya 😂','🥲🥲🥲','Kuch To Log Kahenge 😂 Logo ka kaam Hai Kehna ☺️','😗😗Just Chill Dude 😗😗','😃','🥺','😱😱😱','Tum to bare heavy Driver nikle 😂`])}
  `.trim()
  conn.reply(m.chat, teks, m, { contextInfo: { mentionedJid: [m.sender] }})
  }
- handler.customPrefix = /Bot/
+ handler.customPrefix =  /😒|😎|😂|😆|😅|😂|🤣|😏|😅|😊|😠|😑|🥲|😈|😌|😘|🤨|😙|🥴/i
  handler.command = new RegExp
 
  module.exports = handler
