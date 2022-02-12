d790d7d8d92bec41b23c6af53abe221302123e3a
Toshiro  Hitsugaya <91844428+ToshiroHitsugaya223@users.noreply.github.com>
2022-02-13 00:47:50 +0530

    Add files via upload

---
+++ plugins/hello.js
@@ -0,0 +1,15 @@
+let handler  = async (m, { conn }) => {
+let name = conn.getName(m.sender)
+let teks = `
+${pickRandom([` *Hello👋 @${m.sender.split`@`[0]}_\n\n_How are you😊*`])}
+`.trim()
+conn.reply(m.chat, teks, m, { contextInfo: { mentionedJid: [m.sender] }})
+}
+handler.customPrefix = /Hello/Hey/Hii/hello/Heya/hii/hi/
+handler.command = new RegExp
+
+module.exports = handler
+
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)]
+}
