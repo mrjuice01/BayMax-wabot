354eb77b80effeb9af304fe01b23a18431f508ab
Toshiro  Hitsugaya <91844428+ToshiroHitsugaya223@users.noreply.github.com>
2022-02-13 10:41:21 +0530

    Update how.js

--- plugins/how.js
+++ plugins/how.js
@@ -9,7 +9,7 @@ let handler = async(m, { conn, command, text }) => {
      }
    } : { })
  }
- handler.help = ['gay', 'smart', 'beautiful', 'handsome', 'peat', 'crazy', 'lesbi', 'stress', 'bucin', 'jones', 'sadboy'].  map(v => 'how' + v + 'who?')
+ handler.help = ['gay', 'smart', 'beautiful', 'handsome', 'peat', 'crazy', 'lesbi', 'stress', 'bucin', 'jones', 'sadboy'].  map(v => 'how' + v + '?')
  handler.tags = ['shells']
  handler.command = /^how(gay|smart|beautiful|handsome|gabut|crazy|lesbi|stress?|bucin|jones|sadboy)/i
  handler.owner = false
