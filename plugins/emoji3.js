5d2131f86ecebe532d995dc953788e10aa199bbd
Toshiro  Hitsugaya <91844428+ToshiroHitsugaya223@users.noreply.github.com>
2022-02-13 10:38:21 +0530

    Update emoji3.js

--- plugins/emoji3.js
+++ plugins/emoji3.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, text }) => {
-  conn.reply(m.chat, `${pickRandom(['️Hello', '️Kaise ho', '💖', '💔', '💞', '💕','💓','💜','🐦','or kise ho','sab badaya','Tum Tho heavy driver nikle','chup na bay lavde😈','Auquat mai raho👺','chala ja lavde','mera dil lega','Bolta Kyu Nahi','Bhai dhek mene teko kucn nahi bola','chala ja bacche','pushpa raj jukega nahi sala😼','Jo BTS ke haters hai ga@#£ marav','Grate powers comes with grate responsibility','Ab Dhek','Toshiro ne meko banaya','Toshiro is my owner','Hello world🌍','I am BayMax BOT made with❣️By Toshiro','Thik hai bhai itna he gussa hai tho maar do muje','❣️','op bolte chaddi kholte'])}`.trim(), m)
+  conn.reply(m.chat, `${pickRandom(['️Hello', '️Kaise ho', '💖', '💔', '💞', '💕','💓','💜','🐦','or kise ho','sab badaya✌️','Tum Tho heavy driver nikle','Go and study🙄','Auquat mai raho👺','Hii Babee😘','mera dil lega','Bolta Kyu Nahi','Bhai dhek mene teko kucn nahi bola','chala ja bacche','pushpa raj jukega nahi sala😼','Jo BTS ke haters hai ga@#£ marav','Grate powers comes with grate responsibility','Ab Dhek','Toshiro ne meko banaya','Toshiro is my owner','Hello world🌍','I am BayMax BOT made with❣️By Toshiro','Thik hai bhai itna he gussa hai tho maar do muje','❣️','op bolte chaddi kholte','I❣️u','14th Feb aa raha h kya karoge😏'])}`.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /😒|😎|😂|😆|😅|😂|🤣|😏|😅|😊|😠|😑|🥲|😈|😌|😘|🤨|😙|🥴/i
+handler.customPrefix = /😒|😎|😂|😆|😅|😂|🤣|😏|😅|😊|😠|😑|🥲|😈|😌|😘|🤨|😙|🥴|😁/i
 handler.command = new RegExp
 
 module.exports = handler
