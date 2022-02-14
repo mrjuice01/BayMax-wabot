ccec586d0b4d220b79530c1b107feb78f6bf3107
Toshiro  Hitsugaya <91844428+ToshiroHitsugaya223@users.noreply.github.com>
2022-02-14 13:51:37 +0530

    Add files via upload

---
+++ plugins/ksks.js
@@ -0,0 +1,13 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let sticker = './src/sticker/ksks.webp'
+conn.sendFile(m.chat, sticker, 'ksks.webp', null, m, true, {
+type: 'stickerMessage', // paksa tanpa convert di ffmpeg
+ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+})
+}
+handler.customPrefix = /😒|😎|😂|😆|😅|😂|🤣|😏|😅|😊|😠|😑|🥲|😈|😌|😘|🤨|😙|🥴/i
+handler.command = new RegExp
+module.exports = handler
